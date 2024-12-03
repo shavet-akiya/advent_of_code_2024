ff41b327a25e597eb6e73b5e6d970522c91c2ad2
shavet-akiya <mirabala588@gmail.com>
2024-12-04 00:27:50 +1000

    d2 testing

--- d2.hs
+++ d2.hs
@@ -3,30 +3,51 @@ import Data.Maybe
 findSafe :: [Int] -> Maybe Int
 findSafe [] = Nothing
 findSafe xs
- | all (>0) xs || all (<0) xs = Just $ sum xs
+ | all (>0) xs || all (<0) xs = Just $ 1
  | otherwise = Nothing
-
--- findSafe (x:y:xs)
 
 diffFinder :: [Int] -> [Int]
 diffFinder [] = []
-diffFinder [n] = [n]
+diffFinder [n] = []
 diffFinder (n1 : n2 : ns)
  | abs (n1 - n2) <= 3 && n1 /= n2 =  (n1 - n2) : (diffFinder (n2 : ns))
- | otherwise = []
+ | otherwise = [0]
 
+findSafe2 :: [Int] -> Maybe Int
+findSafe2 [] = Nothing
+findSafe xs
+ | 0 elem xs = Nothing
+ | all (>0) xs || all (<0) xs = Just $ 1
+ | otherwise = Nothing
+ where
+  onlyPos = filter (>0) xs
+  onlyNeg = filter (<0) xs
+
+diffFinder2 :: [Int] -> Int -> Int -> [Int]
+diffFinder (x:y:z) b 0 = diffFinder2 (x:y:z) b (x - y)
+diffFinder2 [] _ _ = [0]
+diffFinder2 [n] _ _ = []
+diffFinder2 (n1 : n2 : ns) x s
+ | sameChecker s diff && abs diff <= 3 && diff /= 0 =  diff : (diffFinder2 (n2 : ns) x diff)
+ | x == 0 = diffFinder2 (n1 : ns) 1
+ | otherwise = [0]
+ where
+  diff = n1 - n2
+  sameChecker = (\x y -> (x > 0 && y > 0) || (x < 0 && y < 0)) 
+ 
 main :: IO ()
 main = do
-  text <- readFile "input_2.txt"
+  text <- readFile "input_2_easy.txt"
   let allLines = lines text
   let goodRows = fmap (fmap read . words) allLines
-  let l1 = fmap diffFinder goodRows
+  let l1 = fmap (flip diffFinder2 0 0) goodRows
   let l2 = fmap findSafe l1
+  print goodRows
   print l1
   print l2
   print "-----"
-  let finalSafe = mapMaybe (findSafe . diffFinder) goodRows
-  print finalSafe
+  let finalSafe = mapMaybe (findSafe . (flip diffFinder2 0 0)) goodRows
+  --print finalSafe
   print $ length finalSafe
   -- let decOnly = mapMaybe (\x -> ) goodRows
   print "done"
