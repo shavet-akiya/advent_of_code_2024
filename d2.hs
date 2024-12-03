b96cef68080f1a299c31079d8b19e98618cf2267
shavet-akiya <mirabala588@gmail.com>
2024-12-03 22:26:50 +1000

    d2 termux code

--- d2.hs
+++ d2.hs
@@ -1,19 +1,24 @@
 import Data.Maybe
 
-findSafe :: [[Int]] -> Int
-findSafe [] = 0
+findSafe :: [Int] -> Maybe Int
+findSafe [] = Nothing
+findSafe xs
+ | all (>0) xs || all (<0) xs = Just $ sum xs
+ | otherwise = Nothing
 
 -- findSafe (x:y:xs)
 
-diffFinder :: [Int] -> [Int] -> Maybe Int
-diffFinder [] d = Just $ length d
-diffFinder [n] d = Just $ length d
-diffFinder (n1 : n2 : ns) d = Just 1 + (n1 - n2) : d
+diffFinder :: [Int] -> [Int]
+diffFinder [] d = d
+diffFinder [n] d = d
+diffFinder (n1 : n2 : ns) d = (n1 - n2) : (diffFinder (n2 : ns))
 
 main :: IO ()
 main = do
   text <- readFile "input_2_easy.txt"
   let allLines = lines text
   let goodRows = fmap words allLines
+  let finalSafe = mapMaybe (findSafe . diffFinder) goodRows
+  print $ length finalSafe
   -- let decOnly = mapMaybe (\x -> ) goodRows
   print "done"
