de46c3d351e0d3b94ba298c0a76b842eaea4c8fb
shavet-akiya <mirabala588@gmail.com>
2024-12-03 23:03:07 +1000

    using proper input d2 s1

--- d2.hs
+++ d2.hs
@@ -15,14 +15,14 @@ diffFinder (n1 : n2 : ns) = (n1 - n2) : (diffFinder (n2 : ns))
 
 main :: IO ()
 main = do
-  text <- readFile "input_2_easy.txt"
+  text <- readFile "input_2.txt"
   let allLines = lines text
   let goodRows = fmap (fmap read . words) allLines
   let l1 = fmap diffFinder goodRows
   let l2 = fmap findSafe l1
-  print l1
-  print l2
-  print "-----"
+  --print l1
+  --print l2
+  --print "-----"
   let finalSafe = mapMaybe (findSafe . diffFinder) goodRows
   print $ length finalSafe
   -- let decOnly = mapMaybe (\x -> ) goodRows
