8f1511badf5d3d2a2179626a0b2d947da35e832e
shavet-akiya <mirabala588@gmail.com>
2024-12-14 22:53:53 +1000

    d3 test

---
+++ d3.hs
@@ -0,0 +1,31 @@
+import Text.Parsec
+import Text.Parsec.String
+
+number :: Parser Integer
+number = read <$> many1 digit
+
+pairser :: Parser (Integer, Integer)
+pairser = do
+  _ <- string "mul("
+  x <- number
+  _ <- char ","
+  y <- number
+  _ <- char ")"
+  return (x,y)
+
+
+allPairser :: Parser[(Integer, Integer)]
+allPairser = many $ skipMany (noneOf "m") *> pairser
+
+muller :: String -> Integer
+muller xs = 
+  case parse allPairser "" xs of
+    Right pairs = do
+      foldl (\(x,y) r -> x*y + r) 0 pairs
+    Left _ = 0
+
+main :: IO ()
+main = do
+  text <- readFile "input_3_easy.txt"
+  print $ muller text
+
